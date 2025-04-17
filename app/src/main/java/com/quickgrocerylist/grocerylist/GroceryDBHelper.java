21f461311367b96451a785fda3a60b24066e5252
mh-rabbi <mhr221official@gmail.com>
2025-04-17 13:14:39 +0600

    activity_main.xml and item_grocery.xml designed and Created GroceryDBHelper class which extends SQLiteOpenHelper

---
+++ app/src/main/java/com/quickgrocerylist/grocerylist/GroceryDBHelper.java
@@ -0,0 +1,24 @@
+package com.quickgrocerylist.grocerylist;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import androidx.annotation.Nullable;
+
+public class GroceryDBHelper extends SQLiteOpenHelper {
+
+    public GroceryDBHelper(@Nullable Context context, @Nullable String name, @Nullable SQLiteDatabase.CursorFactory factory, int version) {
+        super(context, name, factory, version);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
+}
